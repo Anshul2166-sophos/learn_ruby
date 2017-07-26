cfb52892c917aa7671c7f01a4098de652873421d
Anshul Dubey <Anshul Dubey>
2017-07-26 10:13:18 +0530

    Completed the basic methods-add,subtract and sum

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,15 @@
-#write your code here
+def add(num1,num2)
+	sum=num1+num2
+	#{sum}
+end
+def subtract(num1,num2)
+	diff=num1-num2
+	#{diff}
+end
+def sum(array)
+	sum = 0
+	array.each do |x|
+		sum = sum + x
+	end
+	return sum
+end
